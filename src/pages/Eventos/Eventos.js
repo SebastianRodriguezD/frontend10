52b9352b84c46f0eb5df40e71bb399fa752dd1ad
yosoySR <155894514+SebastianRodriguezD@users.noreply.github.com>
2024-10-02 20:08:33 +0200

    correción santi

--- src/pages/Eventos/Eventos.js
+++ src/pages/Eventos/Eventos.js
@@ -2,81 +2,125 @@ import { API } from '../../utils/API/API'
 import { crearPagina } from '../../utils/functions/crearPagina'
 import { Button } from '../../components/Button/Button'
 import './Eventos.css'
+import { navigate } from '../../utils/functions/navigate'
+import { Login } from '../Login/Login'
 
 export const eventos = async () => {
   const div = crearPagina('eventos')
-  const res = await API({
-    endpoint: '/eventos',
-    token: localStorage.getItem('token')
-  })
 
-  const eventosData = res.data
+  const loadingElement = document.createElement('div')
+  loadingElement.textContent = 'Cargando Eventos...'
+  loadingElement.className = 'loading'
+  div.appendChild(loadingElement)
 
-  for (const evento of eventosData) {
-    if (!evento.titulo) {
-      console.error('Título del evento no definido:', evento)
-      continue
+  try {
+    const res = await API({
+      endpoint: '/eventos',
+      token: localStorage.getItem('token')
+    })
+
+    if (loadingElement) {
+      div.removeChild(loadingElement)
     }
 
-    const eventDiv = document.createElement('div')
-    eventDiv.classList.add('evento')
+    const eventosData = res.data
 
-    eventDiv.innerHTML = `
-      <img src="${evento.caratula}" alt="${evento.titulo}">
-      <h3>${evento.titulo}</h3>
-      <p>${evento.fecha}</p>
-      <p>${evento.ciudad}</p>
-    `
+    const asistenciasConfirmadas =
+      JSON.parse(localStorage.getItem('asistencias')) || {}
 
-    const descripcionTexto =
-      evento.info && typeof evento.info === 'string'
-        ? evento.info.substring(0, 200)
-        : 'Descripción no disponible'
-    const descripcion = document.createElement('p')
-    descripcion.textContent = descripcionTexto
-    descripcion.classList.add('descripcion')
-    descripcion.style.display = 'none'
-    eventDiv.appendChild(descripcion)
+    for (const evento of eventosData) {
+      if (!evento.titulo) {
+        console.error('Título del evento no definido:', evento)
+        continue
+      }
 
-    const masInfoButton = Button({
-      text: '+ Info',
-      fnc: () => {
-        if (descripcion.style.display === 'none') {
-          descripcion.style.display = 'block'
-          masInfoButton.textContent = '- Info'
-        } else {
-          descripcion.style.display = 'none'
-          masInfoButton.textContent = '+ Info'
+      const eventDiv = document.createElement('div')
+      eventDiv.classList.add('evento')
+
+      eventDiv.innerHTML = `
+        <img src="${evento.caratula}" alt="${evento.titulo}">
+        <h3>${evento.titulo}</h3>
+        <p>${evento.fecha}</p>
+        <p>${evento.ciudad}</p>
+      `
+
+      const descripcionTexto =
+        evento.info && typeof evento.info === 'string'
+          ? evento.info.substring(0, 200)
+          : 'Descripción no disponible'
+      const descripcion = document.createElement('p')
+      descripcion.textContent = descripcionTexto
+      descripcion.classList.add('descripcion')
+      descripcion.style.display = 'none'
+      eventDiv.appendChild(descripcion)
+
+      const masInfoButton = Button({
+        text: '+ Info',
+        fnc: () => {
+          if (descripcion.style.display === 'none') {
+            descripcion.style.display = 'block'
+            masInfoButton.textContent = '- Info'
+          } else {
+            descripcion.style.display = 'none'
+            masInfoButton.textContent = '+ Info'
+          }
         }
-      }
-    })
-    eventDiv.appendChild(masInfoButton)
+      })
+      eventDiv.appendChild(masInfoButton)
 
-    const confirmarAsistencia = document.createElement('input')
-    confirmarAsistencia.type = 'checkbox'
-    const checkboxTitle = `asistencia-${evento.titulo.replace(/\s+/g, '-')}`
-    confirmarAsistencia.id = checkboxTitle
+      const confirmarAsistencia = document.createElement('input')
+      confirmarAsistencia.type = 'checkbox'
+      const checkboxTitle = `asistencia-${evento.titulo.replace(/\s+/g, '-')}`
+      confirmarAsistencia.id = checkboxTitle
 
-    const asistenciaDiv = document.createElement('div')
-    asistenciaDiv.classList.add('asistencia')
+      confirmarAsistencia.checked =
+        asistenciasConfirmadas[evento.titulo] || false
 
-    const textoAsistencia = document.createElement('span')
-    textoAsistencia.textContent = 'Asistir'
-    textoAsistencia.style.marginLeft = '8px'
+      const asistenciaDiv = document.createElement('div')
+      asistenciaDiv.classList.add('asistencia')
 
-    confirmarAsistencia.addEventListener('change', async (e) => {
-      if (e.target.checked) {
-        alert('¡Asistencia confirmada para el evento: ' + evento.titulo + '!')
-      } else {
-        alert('Has cancelado la asistencia para el evento: ' + evento.titulo)
-      }
-    })
+      const textoAsistencia = document.createElement('span')
+      textoAsistencia.textContent = 'Asistir'
+      textoAsistencia.style.marginLeft = '8px'
 
-    asistenciaDiv.appendChild(confirmarAsistencia)
-    asistenciaDiv.appendChild(textoAsistencia)
+      confirmarAsistencia.addEventListener('change', async (e) => {
+        const existingToken = localStorage.getItem('token')
 
-    eventDiv.appendChild(asistenciaDiv)
+        if (!existingToken) {
+          alert('Debes iniciar sesión para confirmar tu asistencia.')
 
-    div.appendChild(eventDiv)
+          e.target.checked = false
+          navigate(e, { path: '/login', page: Login })
+          return
+        }
+
+        if (e.target.checked) {
+          alert('¡Asistencia confirmada para el evento: ' + evento.titulo + '!')
+          asistenciasConfirmadas[evento.titulo] = true
+        } else {
+          alert('Has cancelado la asistencia para el evento: ' + evento.titulo)
+          delete asistenciasConfirmadas[evento.titulo]
+        }
+
+        localStorage.setItem(
+          'asistencias',
+          JSON.stringify(asistenciasConfirmadas)
+        )
+      })
+
+      asistenciaDiv.appendChild(confirmarAsistencia)
+      asistenciaDiv.appendChild(textoAsistencia)
+
+      eventDiv.appendChild(asistenciaDiv)
+
+      div.appendChild(eventDiv)
+    }
+  } catch (error) {
+    console.error('Error en la solicitud:', error)
+    div.innerHTML = '<p>Error al cargar los datos</p>'
+  } finally {
+    if (div.contains(loadingElement)) {
+      div.removeChild(loadingElement)
+    }
   }
 }
--- src/pages/PostEventos/postEventos.js
+++ src/pages/PostEventos/postEventos.js
@@ -4,9 +4,17 @@ import { crearPagina } from '../../utils/functions/crearPagina'
 import { navigate } from '../../utils/functions/navigate'
 import { doPostEvento } from '../../utils/functions/postEventoApi'
 import { eventos } from '../Eventos/Eventos'
+import { Login } from '../Login/Login'
 import './postEventos.css'
 
 export const postEventos = () => {
+  const existingToken = localStorage.getItem('token')
+  if (!existingToken) {
+    alert('Por favor, inicie sesión para acceder a esta página.')
+    navigate(event, { path: '/login', page: Login })
+    return
+  }
+
   const div = crearPagina('PublicarEvento')
   const form = document.createElement('form')
   const button = Button({
@@ -22,4 +30,5 @@ export const postEventos = () => {
 
   div.append(form)
   div.append(button)
+  return div
 }
